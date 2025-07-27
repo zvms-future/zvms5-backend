21b69e23846c10262a376818a8d13b3ec71e7ddd
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-07-27 15:57:28 +0800

    创建 xuehai.py

---
+++ xuehai.py
@@ -0,0 +1,5 @@
+import requests
+
+TOKEN_VALIDATOR_URL = 'https://ztp.yunzuoye.net/api/v1/pub/distribution/com.xuehai.volunteering/versions/1'
+
+USER_CHECK_URL = 'http://ztp.yunzuoye.cloud/api/v1/platform/users/{}/check'
