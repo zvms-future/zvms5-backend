f69657e9a06b06d772cdb1f0bdb2b526ef51c86e
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-08-23 18:53:01 +0800

    更新 config.py

--- config.py
+++ config.py
@@ -1,1 +1,21 @@
-IS_XUEHAI = True
+# Environment config
+IS_XUEHAI = False # Automatically set in thread, don't edit
+
+# Database config
+DATABASE_URI = 'sqlite:///:memory:'
+
+# Server config
+SERVER_CERT = None
+SERVER_PORT = 9009
+
+# Xuehai env check thread
+from threading import Thread
+def check_zty():
+    global IS_XUEHAI
+    import urllib.request as urllib2
+    try:
+        urllib2.urlopen('http://app.yunzuoye.cloud/health')
+        IS_XUEHAI = True
+    except:
+        pass # Timeout: not on Xuehai server
+Thread(target=check_zty).start()
