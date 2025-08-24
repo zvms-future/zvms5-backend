4af8f41b8525e8c1e8791e0f6da089cc9502929f
Zcraft_ <153985444+Zcraft3793@users.noreply.github.com>
2025-08-24 22:07:36 +0800

    更新 database.py

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from . import config
+from .util import jwt
 
 ObjectId = String(24)
 
@@ -17,20 +18,20 @@ session = Session()
 def Relation(cls, col, **kwargs):
     d = dict(**kwargs)
     d['lazy'] = d.get('lazy', 'dynamic')
-    if 'primaryjoin' in d: delete d['primaryjoin']
-    if 'backref' in d: delete d['backref']
     return relationship(cls, primaryjoin=col + '_obj', backref=col + '_id', **d)
 
 def PrimaryColumn():
     return Column(ObjectId, primary_key=True)
 
-def ObjectRefColumn(name, **kwargs):
-    kw = dict(kwargs)
-    kw['nullable'] = kw.get('nullable', False)
-    return Column(ObjectId, name + '._oid', **kw)
+def ObjectRefColumn(name, **kw):
+    d = dict(kw)
+    d['nullable'] = d.get('nullable', False)
+    return Column(ObjectId, name + '._oid', **d)
 
-def XHRefColumn(name, **kwargs):
-    return Column(Integer, 'xh_' + name + '.id', **kwargs)
+def XHRefColumn(name, **kw):
+    d = dict(kw)
+    d['default'] = d.get('default', -1)
+    return Column(Integer, 'xh_' + name + '.id', **d)
 
 def Timestamp():
     return Column(DateTime, nullable=False)
